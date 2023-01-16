13cd80d7195254f0154301d31086ea670e46eeac
Antonio89743 <99281577+Antonio89743@users.noreply.github.com>
2023-01-16 22:41:56 +0100

    Update replace_text_file_string.sh

--- replace_text_file_string.sh
+++ replace_text_file_string.sh
@@ -1,22 +1,21 @@
 #! usr/bin/bash
 
 # Check if there are 3 parameters
-
 if [ "$#" -ne 3 ]; then
         echo "Error: Send 3 parameters."
         exit
 fi
 
-# check if first parameter is a valid file path
-
+# Check if first parameter is a valid file path
 if ! [ -e $1 ]; then
         echo "File doesn't exist. First parameter has to be a valid file path."
         exit
 fi
 
-## should you check if file access modifier let's you copy?
+## should you check if file access modifier let's you copy from old file?
+## should you check if file changed already exists
 
-# create empty -changed file
+# Create empty -changed file
 directory_path="${1%/*}"
 original_file_name_with_extension="$(basename $1)"
 original_file_name="${original_file_name_with_extension%.*}"
@@ -27,3 +26,5 @@ new_file="$directory_path""/""$original_file_name""_changed.""$extension"
 ## get text from old file
 ## replace args
 ## place the result in the _changed file 
+
+## you could just place func to replace string before >$new_file, so you can create and fill that new_file in one line
